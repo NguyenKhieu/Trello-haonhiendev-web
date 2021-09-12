ad1b1bb6243426149e5d52098c0c4e7d981b715b
unknown <khieuvannguyen97@gmail.com>
2021-09-12 15:21:11 +0700

    clean-code

--- src/App.js
+++ src/App.js
@@ -1,133 +1,18 @@
-
+import React from "react";
 import './App.scss';
+
+//custom components
+import AppBar from 'components/AppBar/AppBar';
+import BoardBar from "components/BoardBar/BoardBar";
+import BoardContent from "components/BoardContent/BoardContent";
+
 
 function App() {
   return (
     <div className="trello-haonhiendev-master">
-      <nav className="navbar app">App Bar</nav>
-      <nav className="navbar board">Board Bar</nav>
-      <nav className="board-columns">
-        <div className="column">
-          <header className="">Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://luv.vn/wp-content/uploads/2021/08/hinh-anh-gai-xinh-2.jpg" alt="haonhiendev-alt-img" />
-              Title: Hao Nhien
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header className="">Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://luv.vn/wp-content/uploads/2021/08/hinh-anh-gai-xinh-2.jpg" alt="haonhiendev-alt-img" />
-              Title: Hao Nhien
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        
-        <div className="column">
-          <header className="">Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://luv.vn/wp-content/uploads/2021/08/hinh-anh-gai-xinh-2.jpg" alt="haonhiendev-alt-img" />
-              Title: Hao Nhien
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        
-        <div className="column">
-          <header className="">Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://luv.vn/wp-content/uploads/2021/08/hinh-anh-gai-xinh-2.jpg" alt="haonhiendev-alt-img" />
-              Title: Hao Nhien
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        
-        <div className="column">
-          <header className="">Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://luv.vn/wp-content/uploads/2021/08/hinh-anh-gai-xinh-2.jpg" alt="haonhiendev-alt-img" />
-              Title: Hao Nhien
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        
-        <div className="column">
-          <header className="">Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://luv.vn/wp-content/uploads/2021/08/hinh-anh-gai-xinh-2.jpg" alt="haonhiendev-alt-img" />
-              Title: Hao Nhien
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        
-        <div className="column">
-          <header className="">Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://luv.vn/wp-content/uploads/2021/08/hinh-anh-gai-xinh-2.jpg" alt="haonhiendev-alt-img" />
-              Title: Hao Nhien
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        
-        <div className="column">
-          <header className="">Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://luv.vn/wp-content/uploads/2021/08/hinh-anh-gai-xinh-2.jpg" alt="haonhiendev-alt-img" />
-              Title: Hao Nhien
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-
-
-      </nav>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
     </div>
   );
 }
